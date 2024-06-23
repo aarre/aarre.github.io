7267cf3048695aa5c432ec1eef5599cd8d6afc97
Aarre Laakso <aarre.laakso@gmail.com>
2024-06-23 14:18:07 -0400

    Whole new approach

--- makewordcloud.py
+++ makewordcloud.py
@@ -1,40 +1,37 @@
-#!/usr/bin/python
+#!venv/bin/python3
 
-import itertools
 import logging
 import re
-import string
 import sys
 
 import matplotlib.pyplot as plt
 import textblob
 import wordcloud
 
-import PyPDF4
-
-# The pyenchant package requires the enchant package, which is only available for Windows, so this script must
-# be run on Linux or Max OS X.
-import pyenchant
+import nltk
+nltk.download('brown')
+nltk.download('punkt')
+nltk.download('words')
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
-def case_insensitive_replace(old_string, new_string, text):
+def case_insensitive_replace(string_to_replace, replacement_string, original_string):
     """
     Replace all instances of old_string with new_string in text regardless of case.
 
-    :param old_string: The original string.
-    :type old_string: str
-    :param new_string: The new string.
-    :type new_string: str
-    :param text: The text in which to perform the replacement.
-    :type text: str
+    :param string_to_replace: The original string.
+    :type string_to_replace: str
+    :param replacement_string: The new string.
+    :type replacement_string: str
+    :param original_string: The text in which to perform the replacement.
+    :type original_string: str
     :return: The text after performing the replacement.
     :rtype: str
 
     See `https://stackoverflow.com/questions/919056/case-insensitive-replace`
     """
-    return re.sub('(?i)' + re.escape(old_string), lambda m: new_string, text)
+    return re.sub('(?i)' + re.escape(string_to_replace), lambda m: replacement_string, original_string)
 
 
 # See https://stackoverflow.com/questions/1157106/remove-all-occurrences-of-a-value-from-a-list
@@ -52,63 +49,84 @@ def write_string_to_file(the_string, filename):
 
 
 # Extract text from PDF
-pdfFileObj = open('_portfolio/scaling_up_romania.pdf', 'rb')
-pdfReader = PyPDF4.PdfFileReader(pdfFileObj)
-text = ''
-for page in pdfReader.pages:
-    text += page.extractText()
-pdfFileObj.close()
-text = text.replace("\n-", "")
+# pdfFileObj = open('_portfolio/scaling_up_romania.pdf', 'rb')
+# pdfReader = PyPDF4.PdfFileReader(pdfFileObj)
+# text = ''
+# for page in pdfReader.pages:
+#     text += page.extractText()
+# pdfFileObj.close()
+# text = text.replace("\n-", "")
+
+# Avoid text from PDFs, which is full of OCR garbage in many cases
+with open('_portfolio/scaling_up_romania.txt', 'r') as file:
+    text = file.read()
 text = text.replace("\n", " ")
-text = text.translate(str.maketrans('', '', string.punctuation))  # Remove all punctuation
+#text = text.translate(str.maketrans('', '', string.punctuation))  # Remove all punctuation
+# Remove short words
+' '.join(word for word in text.split() if len(word)>2)
+# Remove possessives with curly quotes, which show up as their own words with space in them
+text = case_insensitive_replace("’s","", text)
 write_string_to_file(text, "original.txt")
 
-# Drop non-English words
-d = enchant.Dict("en_US")
-english_words = []
-for word in text.split(" "):
-    if (len(word) > 2):
-        if d.check(word):
-            english_words.append(word)
+# # Drop non-English words and "stem" (lemmatize)
+# snow_stemmer = SnowballStemmer(language='english')
+# words = set(nltk.corpus.words.words())
+# text = " ".join(snow_stemmer.stem(w) for w in nltk.wordpunct_tokenize(text) if snow_stemmer.stem(w) in words)
+# write_string_to_file(text, "english.txt")
 
 # Remove consecutive duplicates
 # See https://stackoverflow.com/questions/39237350/how-do-i-remove-consecutive-duplicates-from-a-list
-english_words = [k for k, g in itertools.groupby(english_words)]
+#english_words = [k for k, g in itertools.groupby(english_words)]
 
 # Remove some useless words
-english_words = case_insensitive_remove_values_from_list(english_words, "and")
-english_words = case_insensitive_remove_values_from_list(english_words, "apter")
-english_words = case_insensitive_remove_values_from_list(english_words, "bucharest")
-english_words = case_insensitive_remove_values_from_list(english_words, "figure")
-english_words = case_insensitive_remove_values_from_list(english_words, "manufacture")
-english_words = case_insensitive_remove_values_from_list(english_words, "number")
-english_words = case_insensitive_remove_values_from_list(english_words, "per")
-english_words = case_insensitive_remove_values_from_list(english_words, "practices")
-english_words = case_insensitive_remove_values_from_list(english_words, "romania")
-english_words = case_insensitive_remove_values_from_list(english_words, "ross")
-english_words = case_insensitive_remove_values_from_list(english_words, "source")
-english_words = case_insensitive_remove_values_from_list(english_words, "world bank")
-english_words = case_insensitive_remove_values_from_list(english_words, "world bank group")
+# english_words = case_insensitive_remove_values_from_list(english_words, "and")
+# english_words = case_insensitive_remove_values_from_list(english_words, "apter")
+# english_words = case_insensitive_remove_values_from_list(english_words, "bucharest")
+# english_words = case_insensitive_remove_values_from_list(english_words, "figure")
+# english_words = case_insensitive_remove_values_from_list(english_words, "manufacture")
+
+# english_words = case_insensitive_remove_values_from_list(english_words, "per")
+# english_words = case_insensitive_remove_values_from_list(english_words, "practices")
+# english_words = case_insensitive_remove_values_from_list(english_words, "romania")
+# english_words = case_insensitive_remove_values_from_list(english_words, "ross")
+# english_words = case_insensitive_remove_values_from_list(english_words, "source")
+# english_words = case_insensitive_remove_values_from_list(english_words, "world bank")
+# english_words = case_insensitive_remove_values_from_list(english_words, "world bank group")
 
 # Do some lemmatization by hand
-english_text = " ".join(english_words)
-english_text = case_insensitive_replace("entrepreneurial", "entrepreneurship", english_text)
-english_text = case_insensitive_replace("instruments", "instrument", english_text)
-english_text = case_insensitive_replace("romanian", "romania", english_text)
-english_text = case_insensitive_replace("romania's", "romania", english_text)
-english_text = case_insensitive_replace("scores", "score", english_text)
-english_text = case_insensitive_replace("startups", "startup", english_text)
+#english_text = " ".join(english_words)
+# english_text = text
+# english_text = case_insensitive_replace("entrepreneurial", "entrepreneurship", english_text)
+# english_text = case_insensitive_replace("instruments", "instrument", english_text)
+# english_text = case_insensitive_replace("romanian", "romania", english_text)
+# english_text = case_insensitive_replace("romania's", "romania", english_text)
+# english_text = case_insensitive_replace("scores", "score", english_text)
+# english_text = case_insensitive_replace("startups", "startup", english_text)
+
+# Try to correct spelling
+blob = textblob.blob.TextBlob(text)
+
+#blob.correct() - too slow
 
 # Do automated lemmatization
-blob = textblob.blob.TextBlob(english_text)
-singular_list = [word.lemmatize() for word in blob.words]
-singular_list = remove_values_from_list(singular_list, "wa")  # Somehow introduced by TextBlob
-singular_text = " ".join(singular_list)
-write_string_to_file(singular_text, "singular.txt")
-singular_blob = textblob.blob.TextBlob(singular_text)
+# singular_list = [word.lemmatize() for word in blob.words]
+# singular_list = remove_values_from_list(singular_list, "wa")  # Somehow introduced by TextBlob
+# singular_text = " ".join(singular_list)
+# write_string_to_file(singular_text, "singular.txt")
+# singular_blob = textblob.blob.TextBlob(singular_text)
 
 # Get frequencies of noun phrases
-frequencies = singular_blob.np_counts
+frequencies = blob.np_counts
+
+# Remove short words (again)
+frequencies = {key:frequencies[key] for key in frequencies if len(key)>2}
+
+# Remove distractors
+frequencies.pop("figure", None)
+frequencies.pop("number", None)
+frequencies.pop("romania", None)
+frequencies.pop("romanian", None)
+frequencies.pop("source", None)
 frequencies.pop("world bank", None)
 frequencies.pop("world bank group", None)
 
