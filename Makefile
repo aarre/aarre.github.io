30957d80fcbc51613d20f271f480a2fd9d33745f
Aarre Laakso <aarre.laakso@gmail.com>
2024-06-27 17:18:00 -0400

    Convert image generation from Makefile to Python

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 
-
+ALL_PDF = $(wildcard _portfolio/*.pdf)
+# $(info $$ALL_PDF = ${ALL_PDF})
+ALL_PNG = $(ALL_PDF:.pdf=.png)
+# $(info $$ALL_PNG = ${ALL_PNG})
 
 # Generate static pages and serve them through a website at
 # https://localhost:4000/
 
 serve:
-	bundle exec jekyll serve
+	bundle exec jekyll serve --incremental
 
 
 # Read the Gemfile that lists the gems this application depends on,
@@ -18,6 +21,24 @@ update:
 	bundle update --bundler
 
 
+# The title in this one is in the middle of the page
+_portfolio/digital_entrepreneurship_and_innovation_in_central_america.png:	_portfolio/digital\ entrepreneurship\ and\ innovation\ in\ central\ america.pdf
+	pdftoppm -png -f 1 -l 1 -y 400 -H 800 -singlefile $< > $@
 
-png:	${FILE}.pdf
-	 pdftoppm -png -f 1 -l 1 -singlepage $@
+# The titles in these ones are toward the top of the page
+_portfolio/scaling_up_romania.png:	_portfolio/scaling\ up\ romania.pdf
+	pdftoppm -png -f 1 -l 1 -y 0 -H 800 -singlefile $< > $@
+
+# The titles in these ones are toward the top of the page
+_portfolio/starting_up_romania.png:	_portfolio/starting\ up\ romania.pdf
+	pdftoppm -png -f 1 -l 1 -y 0 -H 800 -singlefile $< > $@
+
+# Make PNG files from PDF, general case
+# %.png: %.pdf
+#	pdftoppm -png -f 1 -l 1 -y 400 -H 800 -singlefile $< > $@
+
+.PHONY:	clean
+
+clean:
+	$(RM) $(ALL_PNG) ;\
+
--- makepng.py
+++ makepng.py
@@ -1,17 +1,53 @@
-#!/usr/bin/python
+#!env/bin/python
+"""
+Make PNG files from PDF files in the PDF directory.
 
-import os
+I tried using make to do this, but it has a number of problems.
+Among them:
+
+    (1) I cannot get make to coqllect a list of PDF files in the
+        _portfolio directory and make the PNG files
+        for all of them. I can just do one at a time.
+
+    (2) Make does not deal well with spaces in filenames, but I
+        have to put spaces in these filenames or Hydejack puts
+        the underscores on my web page, which is ugly.
+"""
+import logging
 import pathlib
 import subprocess
-import sys
+
+logger = logging.getLogger(__name__)
+
+
+def make_one_png(pdf_path_string: str, y_offset: int, height: int):
+    """
+    Make a single PNG file from a single PDF file.
+
+    :param pdf_path_string: Path to the PDF file.
+    :param y_offset: How many pixels to offset the PNG image from the
+           top of the PDF file.
+    :param height: How many pixels high the PNG should be.
+    """
+    assert pdf_path_string.endswith("pdf")
+    pdf_path = pathlib.Path(pdf_path_string)
+    png_path = pdf_path.with_suffix('.png')
+    png_contents = subprocess.check_output(["pdftoppm", "-png",
+                                            "-f", "1",
+                                            "-l", "1",
+                                            "-y", str(y_offset),
+                                            "-H", str(height),
+                                            "-singlefile",
+                                            str(pdf_path)])
+    with open(png_path, 'wb') as png_file:
+        png_file.write(png_contents)
+    png_file.close()
+
 
 if __name__ == "__main__":
-
-    pdf_filename = sys.argv[1]
-    assert pdf_filename.endswith("pdf")
-    png_filename = pathlib.Path(pdf_filename).with_suffix('.png')
-    output = subprocess.check_output(["pdftoppm", "-png", "-f", "1", "-l", "1", "-singlefile", pdf_filename])
-    with open(png_filename, 'wb') as f:
-        f.write(output)
-    f.close()
-
+    logging.basicConfig(level=logging.INFO)
+    logger.info("Starting up")
+    make_one_png("_portfolio/digital entrepreneurship and "
+                 "innovation in central america.pdf", 400, 800)
+    make_one_png("_portfolio/scaling up romania.pdf", 0, 800)
+    make_one_png("_portfolio/starting up romania.pdf", 0, 800)
