6b514d4b3cc580b81233187d4bbc141724befc9b
Aarre Laakso <aarre.laakso@gmail.com>
2024-06-29 11:19:31 -0400

    Move most tools ../tools

--- troubleshooting.py
+++
@@ -1,7 +0,0 @@
-#!venv/bin/python
-
-import platform; print(platform.platform())
-import sys; print("Python", sys.version)
-import numpy; print("NumPy", numpy.__version__)
-import matplotlib; print("matplotlib", matplotlib.__version__)
-import wordcloud; print("wordcoud", wordcloud.__version__)
